b22bde1336e3e6eaeb2d77cdb3924a530980fdf1
Shreejan Joshi <shreejanjoshi97@gmail.com>
2024-10-25 13:14:01 +0200

    Addes models like club, city and country

---
+++ Basketball/Basketball/Models/BasketballClub.cs
@@ -0,0 +1,9 @@
+﻿namespace Basketball.Models
+{
+    public class BasketballClub
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CityId { get; set; } 
+    }
+}
