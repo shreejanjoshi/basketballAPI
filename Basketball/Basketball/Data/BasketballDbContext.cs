b8687ee7fbc4c136b970039e3648a56e94c2422e
Shreejan Joshi <shreejanjoshi97@gmail.com>
2024-12-05 14:28:37 +0100

    created basketballdbcontext for connection with database

---
+++ Basketball/Basketball/Data/BasketballDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Basketball.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basketball.Data
+{
+    public class BasketballDbContext : DbContext
+    {
+        public DbSet<BasketballClub> BasketballClubs => Set<BasketballClub>();
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+
+        public BasketballDbContext(DbContextOptions<BasketballDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Grouping the configurations
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataDbContext).Assembly);
+        }
+    }
+}
