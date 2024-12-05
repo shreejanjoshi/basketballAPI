9e52a8b16caccf70cd75b6aa37ced18f67b0af94
Shreejan Joshi <shreejanjoshi97@gmail.com>
2024-12-05 16:39:08 +0100

    upadate program class

--- Basketball/Basketball/Program.cs
+++ Basketball/Basketball/Program.cs
@@ -1,5 +1,8 @@
 
+using Basketball.Data;
 using Basketball.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace Basketball
 {
@@ -11,23 +14,42 @@ namespace Basketball
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services
+                .AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // for migartions
-            //builder.Services.AddDbContext<DataDbContext>(options =>
-            //{
-            //    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            //    var serverVersion = ServerVersion.AutoDetect(connectionString);
+            // voor sql connection
+            builder.Services.AddDbContext<BasketballDbContext>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                var serverVersion = ServerVersion.AutoDetect(connectionString);
 
-            //    options.UseMySql(connectionString, serverVersion);
-            //});
+                options.UseMySql(connectionString, serverVersion);
+            });
 
             builder.Services.AddSingleton<IBasketballClubService, BasketballClubService>();
             builder.Services.AddSingleton<ICityService, CityService>();
             builder.Services.AddSingleton<ICountryService, CountryService>();
+
+            // Configure DI to switch between in-memory and database implementations
+            if (builder.Configuration["UseInMemory"] == "true")
+            {
+                builder.Services.AddSingleton<IBasketballClubService, BasketballClubService>();
+                builder.Services.AddSingleton<ICityService, CityService>();
+                builder.Services.AddSingleton<ICountryService, CountryService>();
+            }
+            else
+            {
+                builder.Services.AddScoped<IBasketballClubService, BasketballClubDatabaseService>();
+                builder.Services.AddScoped<ICityService, CityDatabaseService>();
+                builder.Services.AddScoped<ICountryService, CountryDatabaseService>();
+            }
 
             var app = builder.Build();
 
